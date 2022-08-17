358171cf4b47579d4d73077d8f379caaf54fb7da
Денис Бабоченко <babochenko.denis2@gmail.com>
2022-08-17 23:44:59 +0300

    re-render window on reopen on macos

--- electron.js
+++ electron.js
@@ -22,6 +22,13 @@ app.whenReady().then(() => {
   createWindow()
 })
 
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     serverProcess.kill();
